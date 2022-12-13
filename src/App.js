4a553c215d414da9a66c7834cb16c84600e5b251
Srbh6 <msaurabh074@gmail.com>
2022-12-13 18:36:45 +0530

    props commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from "react";
 function App() {
   return (
     <div>
-     
-      
+      <h1> Hi This is App</h1>
     </div>
   );
 }
---
+++ src/Learn/Props.jsx
@@ -0,0 +1,8 @@
+// what is props
+
+// what is props
+// Props are arguments passed into React components.
+// Props are passed to components via HTML attributes.
+// props stands for properties.
+// we can no change the value with props are read only ( if we want to change the value we need to change the props into state then we can do )
+//  passing the argument from parents tp child(supposed we want to greet every student in the class we only pass name thats it.
