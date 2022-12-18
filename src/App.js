b7ac4012957a0a669407fb9d809e719121d3aa6e
Srbh6 <msaurabh074@gmail.com>
2022-12-18 17:08:31 +0530

    Props commit with code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import App1 from './Learn/State'
+// import App1 from './Learn/State'
+import Student from './Learn/Props'
 function App() {
   return (
     <div >
   
-    <App1 />
+    {/* <App1 /> */}
+    
+    <Student name= {"Saurabh"} email= "msaurabh074@gmail.com" />
+    <Student name= {"ironman"} />
+    <Student name= {"sam"} />
+    <Student />
     </div>
   )
 }
--- src/Learn/Props.jsx
+++ src/Learn/Props.jsx
@@ -6,3 +6,17 @@
 // props stands for properties.
 // we can no change the value with props are read only ( if we want to change the value we need to change the props into state then we can do )
 //  passing the argument from parents tp child(supposed we want to greet every student in the class we only pass name thats it.
+
+import React from "react";
+function Student(X) {
+   
+  return (
+    <div> 
+        gi
+          <h1> hello {X.name}</h1>
+          <h2> Email : {X.email}</h2>
+
+      </div>
+  );
+}
+export default Student;
