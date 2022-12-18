9a552e6d3ad4c278ecc82bde9104de6af1345864
Srbh6 <msaurabh074@gmail.com>
2022-12-18 15:09:07 +0530

    State commit

---
+++ src/Learn/State.jsx
@@ -0,0 +1,20 @@
+// what is state 
+// state is internal object of class can not accessible outside from class object : key
+// state are changeable.
+// state example
+// button click update
+
+
+
+
+
+// State is used to store the data of the
+// ‘components that has to be rendered to the view
+
+// State holds the data and can change over
+// time
+
+// State can only be used in class components,
+
+// State is generally updated by event handlers
+// g
